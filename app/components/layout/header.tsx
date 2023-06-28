8c25684f83dc99cd2f78ebc8d18c73997cc3ab97
Naufaldi Rafif S <naufaldi.rafif@efishery.com>
2023-06-28 15:53:13 +0700

    feat(icons): add MenuClose component
    feat(header): add mobile menu functionality with slide animation and close button
    
    feat(theme): add global styles for smooth scrolling
    
    The changes were made to add a new icon component called MenuClose, which is used in the header component to toggle the mobile menu. The header component now includes a mobile menu with slide animation and a close button. Additionally, the theme now includes global styles for smooth scrolling.

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -1,26 +1,63 @@
-import { Box, Button, Container, Flex, HStack, Link as ChakraLink } from '@chakra-ui/react'
-import { Link } from '@remix-run/react'
+import { Box, Button, Container, Flex, HStack, Link as ChakraLink, useDisclosure, Slide } from '@chakra-ui/react'
+import { Link as RemixLink } from '@remix-run/react'
 
 import Menu from '../icons/home/Menu'
 import Logo from '../icons/logo'
 import MessagesIcon from '../icons/messages'
+import MenuClose from '../icons/home/MenuClose'
+import { useEffect, useState } from 'react'
+
+interface listMenuMobileProps {
+  link: string,
+  caption: string
+}
 
 function Header() {
+  const { isOpen, onToggle } = useDisclosure();
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+
+  const listMenuMobile: listMenuMobileProps[] = [
+    {
+      link: '#about',
+      caption: 'About'
+    },
+    {
+      link: '#services',
+      caption: 'Services'
+    },
+    {
+      link: '#portfolio',
+      caption: 'Portfolio'
+    },
+
+  ]
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentPosition = window.scrollY || document.documentElement.scrollTop;
+      setScrollPosition(currentPosition);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <Box as="header" w="full" py="8px" position="fixed" top="0" right="0" left="0" bg="white" zIndex="3">
+    <Box as="header" w="full" py="8px" position="fixed" top="0" right="0" left="0" bg="white" zIndex="3"
+      boxShadow={scrollPosition > 10 ? 'lg' : 'none'}
+    >
       <Container maxW="1230px" mx="auto" w="full">
-        <Flex w="full" justifyContent="space-between" alignItems="center">
-          <Logo w="122px" h="64px" />
+        <Flex w="full" justifyContent="space-between" alignItems="center" >
+          <Logo w="122px" h="64px" zIndex={'15'} />
           <HStack spacing="56px" display={{ base: 'none', sm: 'flex' }}>
-            <Link to="/">
-              <ChakraLink p="8px">About</ChakraLink>
-            </Link>
-            <Link to="/">
-              <ChakraLink p="8px">Services</ChakraLink>
-            </Link>
-            <Link to="/">
-              <ChakraLink p="8px">Portfolio</ChakraLink>
-            </Link>
+            <ChakraLink as={RemixLink} to="/" p="8px">About</ChakraLink>
+            <ChakraLink as={RemixLink} to="/" p="8px">Services</ChakraLink>
+            <ChakraLink as={RemixLink} to="/" p="8px">Portfolio</ChakraLink>
           </HStack>
           <Button
             display={{ base: 'none', sm: 'flex' }}
@@ -36,11 +73,48 @@ function Header() {
             Contact Us
             <MessagesIcon ml="8px" boxSize="24px" />
           </Button>
-          <Button width={"32px"} height={'32px'} border="0">
-            <Menu boxSize="32px" />
+          <Button display={{ base: 'flex', sm: 'none' }} width={"32px"} height={'32px'} zIndex={'15'} border="0" onClick={onToggle}>
+            {
+              !isOpen ? <Menu boxSize="32px" /> : <MenuClose boxSize="32px" />
+            }
           </Button>
         </Flex>
       </Container>
+      {/* Menu Dropdown Mobile */}
+      <Slide direction='top' in={isOpen} style={{ zIndex: 10 }}>
+        <Box
+          mt="80px"
+          bg="white"
+          py="32px"
+          px="80px"
+          h="470px"
+        >
+          <Flex flexDir={'column'} gap="56px" alignItems={'center'}>
+            {
+              listMenuMobile.map((item, index) => (
+                <ChakraLink as={RemixLink} key={index} textAlign={'center'} onClick={onToggle} to={item.link} py="12px" maxW="210px" w="full" >
+                  {item.caption}
+                </ChakraLink>
+              ))
+            }
+            <Button
+              display={{ base: 'none', sm: 'flex' }}
+              py="12px"
+              maxW="192px"
+              w="full"
+              color="brand.secondary"
+              fontWeight="500"
+              fontSize="base"
+              bg="white"
+              border="1px solid #A5B3CF"
+            >
+              Contact Us
+              <MessagesIcon ml="8px" boxSize="24px" />
+            </Button>
+          </Flex>
+
+        </Box>
+      </Slide>
     </Box>
   )
 }
--- app/styles/theme.ts
+++ app/styles/theme.ts
@@ -3,6 +3,13 @@ import { extendTheme } from '@chakra-ui/react'
 import { Button } from './button/index'
 
 const theme = extendTheme({
+  styles: {
+    global: {
+      'html, body': {
+        scrollBehavior: 'smooth',
+      },
+    },
+  },
   fonts: {
     heading: `'Inter', sans-serif`,
     body: `'Inter', sans-serif`,
